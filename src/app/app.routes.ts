8f7cdcc0a280fd6f4e2a67ef0ece592bcae5b75f
Furmiga2306 <gustavolicon23062003@gmail.com>
2024-12-21 17:29:10 -0300

    commit1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './paginas/login/login.component';
-import { ContratarComponent } from './components/contratar/contratar.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
@@ -11,9 +10,6 @@ export const routes: Routes = [
   {
     path: "login",
     component: LoginComponent
-  },
-  {
-    path: "contratar",
-    component: ContratarComponent
   }
+
 ]
